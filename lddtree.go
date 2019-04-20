bedd196c67997dec292455502a12e18fffda77fd
navigaid <navigaid@gmail.com>
2019-04-21 01:07:52 +0800

    add detailed usage info; change ResolveIndent format

--- lddtree.go
+++ lddtree.go
@@ -8,12 +8,17 @@ import (
 
 func main() {
 	if len(os.Args) <= 1 {
-		log.Fatalln("usage: ./lddtree [file]")
+		log.Fatal(`usage: ./lddtree [file]
+[file] is searched in such order:
+- /etc/ld.so.cache
+- [file], interpreted as relative path
+- PATH
+`)
 	}
 	root := New(os.Args[1])
 	//root.Resolve()
 	fmt.Println("============================= .ResolvIndent() =============================")
-	root.ResolveIndent("    ")
+	root.ResolveIndent(".")
 
 	fmt.Println("============================= .String() =============================")
 	fmt.Print(root.String())
